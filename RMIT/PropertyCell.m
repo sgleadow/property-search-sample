b00fa914e9cb22c0fc5a8734fd6e6fef95761ef0
Stewart Gleadow <sgleadow@thoughtworks.com>
2011-09-10 08:38:37 +1000

    Removed top/bottom shadows on the cell

--- RMIT/PropertyCell.m
+++ RMIT/PropertyCell.m
@@ -22,16 +22,6 @@
         
         self.selectedBackgroundView = [[[UIView alloc] initWithFrame:CGRectZero] autorelease];
         self.selectedBackgroundView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"texture-highlight"]];
-        
-        UIView *topShadow = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"shadow-top"]] autorelease];
-        topShadow.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-        [self.selectedBackgroundView addSubview:topShadow];
-        
-        UIImage *bottom = [UIImage imageNamed:@"shadow-bottom"];
-        UIView *bottomShadow = [[[UIImageView alloc] initWithImage:bottom] autorelease];
-        bottomShadow.frame = CGRectMake(0, -bottom.size.height, bottom.size.width, bottom.size.height);
-        bottomShadow.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin;
-        [self.selectedBackgroundView addSubview:bottomShadow];
     }
     
     return self;
