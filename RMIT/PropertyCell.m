be74e9db7a5a2f4827e0f40b1a94d69d082b2154
Stewart Gleadow <sgleadow@thoughtworks.com>
2011-09-13 13:25:54 +1000

    Configured text label properties

--- RMIT/PropertyCell.m
+++ RMIT/PropertyCell.m
@@ -20,7 +20,18 @@
                      reuseIdentifier:reuseIdentifier]))
     {
         self.textLabel.textColor = [UIColor darkGrayColor];
+        self.textLabel.highlightedTextColor = [UIColor darkGrayColor];
         self.textLabel.backgroundColor = [UIColor clearColor];
+        self.textLabel.font = [UIFont boldSystemFontOfSize:14];
+        self.textLabel.shadowColor = [UIColor whiteColor];
+        self.textLabel.shadowOffset = CGSizeMake(0, 1);
+        
+        self.detailTextLabel.textColor = [UIColor lightGrayColor];
+        self.detailTextLabel.highlightedTextColor = [UIColor lightGrayColor];
+        self.detailTextLabel.backgroundColor = [UIColor clearColor];
+        self.detailTextLabel.font = [UIFont boldSystemFontOfSize:12];
+        self.detailTextLabel.shadowColor = [UIColor whiteColor];
+        self.detailTextLabel.shadowOffset = CGSizeMake(0, 1);
         
         self.backgroundView = [[[PropertyCellBackgroundView alloc]
                                initWithImageName:@"texture"] autorelease];
