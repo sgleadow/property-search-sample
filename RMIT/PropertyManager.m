b7dd69724be739caa9a019b4acf92df5662697be
Jesse Collis <jesse@jcmultimedia.com.au>
2011-09-20 14:48:53 +1000

    Property Manager Complete

--- RMIT/PropertyManager.m
+++ RMIT/PropertyManager.m
@@ -18,26 +18,39 @@
 {
     //More on singletons can be found in 'Cocoa Design Patterns' by Erik Buck & Donald Yacktman Chapter 13, page 148 of te 2010 edition.
 
-//    static PropertyManager *singletonInstance = nil;
+    static PropertyManager *singletonInstance = nil;
 
-//    if (!singletonInstance)
-//    {
-//        singletonInstance = [[[self class] alloc] init];
-//    }
-//    return singletonInstance;
-  return nil;
+    if (!singletonInstance)
+    {
+        singletonInstance = [[[self class] alloc] init];
+    }
+    return singletonInstance;
 }
 
 - (void)performPropertySearch:(NSDictionary *)params
 {
-  
+    [[LRResty client] get:@"http://rmit-property-search.heroku.com/search"
+               parameters:params
+                 delegate:self];
 }
 
 #pragma mark - LRRestyClientResponseDelegate
 
 - (void)restClient:(LRRestyClient *)client receivedResponse:(LRRestyResponse *)response
 {
-
+    NSData *data = [response responseData];
+    
+    NSDictionary *jsonDictionary = [data yajl_JSON];
+    NSArray *propertiesArray = [jsonDictionary valueForKey:@"properties"];
+    
+    NSMutableArray *newProperties = [NSMutableArray array];
+    for (NSDictionary *dict in propertiesArray)
+    {
+        Property *property = [Property propertyWithDictionary:dict];
+        [newProperties addObject:property];
+    }
+    
+    self.properties = newProperties;
 }
 
 @end
